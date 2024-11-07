18059f4be74e89136c4be4b807289d2e25b96e91
BSHkr <tmdgh784@naver.com>
2024-11-08 02:58:21 +0900

    Add Robot Moving API

---
+++ src/main/java/com/example/capstone/dto/request/RobotMoveRequest.java
@@ -0,0 +1,11 @@
+package com.example.capstone.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RobotMoveRequest {
+    private double x;
+    private double y;
+}
