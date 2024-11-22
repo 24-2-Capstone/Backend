179dbde8ab803dacc53fac436cd0dccff9ec5806
BSHkr <tmdgh784@naver.com>
2024-11-22 15:13:02 +0900

    update

--- src/main/java/com/example/capstone/dto/response/LocationDto.java
+++ src/main/java/com/example/capstone/dto/response/LocationDto.java
@@ -1,5 +1,6 @@
 package com.example.capstone.dto.response;
 
+import com.example.capstone.entity.Category;
 import com.example.capstone.entity.Location;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,19 +9,22 @@ import lombok.Getter;
 public class LocationDto {
 
     private Long id;
+    private String name;
     private int x;
     private int y;
 
     @Builder
-    public LocationDto(Long id, int x, int y) {
+    public LocationDto(Long id, String name, int x, int y) {
         this.id = id;
+        this.name = name;
         this.x = x;
         this.y = y;
     }
 
-    public static LocationDto toLocationDto(Location location) {
+    public static LocationDto toLocationDto(Location location, Category category) {
         return LocationDto.builder()
                 .id(location.getId())
+                .name(category.getCategoryName())
                 .x(location.getX())
                 .y(location.getY())
                 .build();
